df9fc6a5d073598c982928c337e79d93eb4e90fc
rabbanisyab <rabbanihaya@gmail.com>
2023-10-26 13:29:14 +0700

    menampilkan list product

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="w-full border-b border-slate-200 py-4">
+      <div class="container">
+        <div class="flex justify-between items-center">
+          <div>
+            <NuxtLink to="/" class="text-2xl font-bold ml-4">SMK
+              Coding</NuxtLink>
+          </div>
+          <nav class="flex items-center gap-8 mr-6">
+            <NuxtLink to="/" class="text-base">Home</NuxtLink>
+            <NuxtLink to="/product" class="textbase">Products</NuxtLink>
+            <NuxtLink to="/cart" class="text-base">Cart</NuxtLink>
+            <button class="btn btn-primary text-white ml-4">Login/Sign Up</button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  </template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="hero bg-base-200 min-h-screen flex flex-col">
+        <div class="w-[600px] mr-[60px]">
+          <h1 class="text-5xl font-bold">Selamat Datang di SMKCoding Store!</h1>
+          <p class="py-6 w-[500px]">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+            id nisi.
+          </p>
+          <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+  </template>
+  
